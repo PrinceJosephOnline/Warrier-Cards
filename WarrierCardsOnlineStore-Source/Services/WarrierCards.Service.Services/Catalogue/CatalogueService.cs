6abdbcd05a7a5818c9b8022acf4c372766eb762b
PrinceJosephOnline <prince.myoffice@gmail.com>
2016-09-02 16:51:00 +0530

    Untracked files

---
+++ WarrierCardsOnlineStore-Source/Services/WarrierCards.Service.Services/Catalogue/CatalogueService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarrierCards.Service.DataObjects;
+
+namespace WarrierCards.Service.Services.Catalogue
+{
+    public class CatalogueService : ICatalogueService
+    {
+        public List<CardBasicInfo> GetCardsList(SearchCriteria searchCriteria)
+        {
+            // TODO: Dummy method here
+            return DummyData.GetDummyData(searchCriteria);
+        }
+
+        public CardDetails GetCardDetailsById(int cardId)
+        {
+
+            return DummyData.GetCardDetailsById(cardId);
+        }
+
+        public CardQuickViewData GetCardQuickViewData(int cardId)
+        {
+            return DummyData.GetCardQuickViewData(cardId);
+        }
+
+        public List<SelectedCardInfo> GetCardsInfoByIds(List<SelectedCard> selectedCards)
+        {
+            List<SelectedCardInfo> selectedCardInfos = new List<SelectedCardInfo>();
+            var selectedCardIds = selectedCards.Select(x => x.CardId);
+            var cardBasicInfos = DummyData.Cards.Where(c => selectedCardIds.Contains(c.CardId)).ToList();
+            foreach (var card in cardBasicInfos)
+            {
+                var cardInfo = new SelectedCardInfo();
+                cardInfo.Card = card;
+                cardInfo.Quantity = selectedCards.Find(c => c.CardId == card.CardId).Quantity;
+                selectedCardInfos.Add(cardInfo);
+            }
+
+            return selectedCardInfos;
+        }
+
+        public bool SendEnquiry(EnquiryData enquiry)
+        {
+            return true;
+        }
+
+
+    }
+}
---
+++ WarrierCardsOnlineStore-Source/Services/WarrierCards.Service.Services/Catalogue/ICatalogueService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WarrierCards.Service.DataObjects;
+
+namespace WarrierCards.Service.Services.Catalogue
+{
+    [ServiceContract]
+    public interface ICatalogueService
+    {
+        [OperationContract]
+        List<CardBasicInfo> GetCardsList(SearchCriteria searchCriteria);
+
+        [OperationContract]
+        CardDetails GetCardDetailsById(int cardId);
+
+        [OperationContract]
+        CardQuickViewData GetCardQuickViewData(int cardId);
+
+        [OperationContract]
+        List<SelectedCardInfo> GetCardsInfoByIds(List<SelectedCard> selectedCards);
+
+        [OperationContract]
+        bool SendEnquiry(EnquiryData enquiry);
+
+        // Get by list of card IDs- for short list, cart views
+
+        // Add to short list
+        // Add to cart
+    }
+}
